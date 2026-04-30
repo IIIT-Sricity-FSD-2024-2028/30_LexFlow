5f4a8f1656bd237bd058f387b4b584cc2ec7e0d7
Devashish <pundkardevashish@gmail.com>
2026-04-30 18:47:48 +0530

    Modified structure and added modules

---
+++ back-end/src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConsultationsModule } from './consultations/consultations.module';
+import { CasesModule } from './cases/cases.module';
+import { DocumentsModule } from './documents/documents.module';
+import { BillingModule } from './billing/billing.module';
+
+@Module({
+  imports: [UsersModule, ConsultationsModule, CasesModule, DocumentsModule, BillingModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
